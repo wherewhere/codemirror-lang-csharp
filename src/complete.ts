802aba9003032446b3913b12af49b3936efb705e
where where <qq2518766683@outlook.com>
2026-04-22 12:01:20 +0800

    Fix completion type

--- src/complete.ts
+++ src/complete.ts
@@ -1,8 +1,12 @@
 import { completeFromList, ifNotIn } from "@codemirror/autocomplete";
 import { atoms, keywords, types } from "./keywords";
 
+type CompletionType = "class" | "constant" | "enum" |
+    "function" | "interface" | "keyword" | "method" | "namespace" |
+    "property" | "text" | "type" | "variable";
+
 export function csharpCompletion() {
-    function map(type: string) {
+    function map(type: CompletionType) {
         return (label: string) => { return { label, type }; }
     }
     return ifNotIn([
@@ -12,5 +16,5 @@ export function csharpCompletion() {
         "LineComment", "BlockComment", "DocComment",
         "PP_Directive", "ArgumentName", "AttrsNamedArg",
         "ConstName", "MethodName", "ParamName", "VarName", "FieldName", "PropertyName"],
-        completeFromList(keywords.map(map("keywords")).concat(types.map(map("type")), atoms.map(map("constant")))));
+        completeFromList(keywords.map(map("keyword")).concat(types.map(map("type")), atoms.map(map("constant")))));
 }
